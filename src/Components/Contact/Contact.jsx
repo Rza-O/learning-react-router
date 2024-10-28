31e7f25fbffd43dc1193d286119922cff9bad7bd
appoclayptor <shahreza1414@gmail.com>
2024-10-29 01:24:15 +0600

    first commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>We are One Call Away</h2>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <span>My Website</span>
+                <br />
+                <Link to='/'>Home</Link>
+                <Link to='/users'>Users</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/contact'>Contact</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
